41684a06490730c3561511586d4b9ba3163d5ca2
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-04-13 17:10:38 +0800

    fix: fix headless CICD

--- Main/Boundary/Init.java
+++ Main/Boundary/Init.java
@@ -37,7 +37,7 @@ public class Init extends JFrame implements ActionListener {
         registerButton.addActionListener(this);
         loginButton.addActionListener(this);
 
-        System.out.println("Compiled Successfully in HEADLESS enviroment");
+        System.out.println("Compiled Successfully");
         
     }
 
