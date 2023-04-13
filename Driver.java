bb7f4d97070787be214acb441720eeed759d5e8d
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-04-13 16:06:07 +0800

    feat: trying to solve headless CICD

--- Driver.java
+++ Driver.java
@@ -1,12 +1,24 @@
 import Main.Boundary.*;
 import java.awt.HeadlessException;
 
+import Database.DB;
+import java.sql.Connection;
+
 public class Driver {
 	public static void main(String[] args) {
-		try {
-			new Init();
-		} catch (HeadlessException e) {
-			System.out.println("Compiled Successfully in HEADLESS enviroment");
-		}
+		new Init();
+		// try {
+		// 	new Init();
+		// } catch (HeadlessException e) {
+		// 	System.out.println("Compiled Successfully in HEADLESS enviroment");
+		// 	DB db = new DB("TestDB");
+		// 	Connection conn = db.getConnection();
+
+		// 	if (conn != null) {
+		// 		System.out.println("[+] Connected to <" + "TestDB" + "> database");
+		// 	} else {
+		// 		System.out.println("[!] Failed to connect to <" + "TestDB" + "> database");
+		// 	}
+		// }
 	}
 }
