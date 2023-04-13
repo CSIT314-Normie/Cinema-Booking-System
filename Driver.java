0cf7ce40bbc188a429ac1ace38f3d2a1ef5c35f6
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-04-13 11:10:29 +0800

    fix: headless issue with CICD

--- Driver.java
+++ Driver.java
@@ -3,7 +3,7 @@ import Main.Boundary.*;
 
 public class Driver {
 	public static void main(String[] args) {
-		System.setProperty("java.awt.headless", "true");
+		System.setProperty("java.awt.headless", "false");
 		new Init();
 	}
 }
