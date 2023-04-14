7f02644f3d137e7cb288463ddcbb3640b63c26f1
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-04-14 19:00:02 +0800

    fix: modified Register Controller to fit into OOP

--- Main/Controller/RegisterController.java
+++ Main/Controller/RegisterController.java
@@ -1,24 +1,24 @@
 package Main.Controller;
 
-import Main.Entity.User;
+import Main.Entity.*;
 
 import java.util.*;
 
+
 public class RegisterController {
-    private static User user = new User();
+    private Customer customer;
+
+    public RegisterController(ArrayList<String> values, String role) {
+        customer = new Customer(values.get(0), values.get(1), values.get(2), values.get(3), values.get(4), role);
+    }
+  
 
     public boolean createUser(ArrayList<String> values, String role) {
-        return user.createUser(values, role);
+        return this.customer.createUser(values, role);
     }
 
     public boolean updateUser(ArrayList<String> values, String role) {
-        return user.updateUser(values, role);
+        return this.customer.updateUser(values, role);
     }
 
-    public boolean deleteUser(ArrayList<String> values, String role) {
-        return user.deleteUser(values, role);
-    }
-
-    
-    
 }
