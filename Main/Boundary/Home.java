8a42b1e78c106f669b04cccf175428b055b5e4dd
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-04-18 16:43:35 +0800

    feat: added Homepage skeleton

---
+++ Main/Boundary/Home.java
@@ -0,0 +1,50 @@
+package Main.Boundary;
+
+import Main.Controller.LogoutController;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.util.*;
+
+
+public class Home extends JFrame implements ActionListener {
+    JButton logoutButton = new JButton("Logout");
+
+    public Home(String userRole) {
+        super("Welcome to CSIT 314 Cinema Booking System - Home");
+        setLayout(new FlowLayout());
+        setSize(1035, 750);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        // TODO: Modify homepage based on user role
+        System.out.println(userRole);
+
+        // Logout button
+        logoutButton.setBounds(500, 400, 100, 50);
+
+        // add buttons to the frame
+        add(logoutButton);
+
+        // add action listener to the buttons
+        logoutButton.addActionListener(this);
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Logout":
+                dispose();
+                new Login();
+                System.out.println("[+]Successfully logged out");
+                break;
+        }
+    }   
+
+}
