76d9b39ce5543022c521fe7f4e0a15a481937900
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-04-21 19:24:52 +0800

    feat: added user admin acc at init

--- Database/DB.java
+++ Database/DB.java
@@ -36,7 +36,7 @@ public class DB {
             System.out.println("[+] Connected to the database on initialisation");
             
             ArrayList<PreparedStatement> stmts = new ArrayList<>();
-            String[] tables = {"users", "movies", "user_movies"};
+            String[] tables = {"users", "movies", "user_movies", "add Admin"};
 
             stmts.add(conn.prepareStatement("CREATE TABLE IF NOT EXISTS users ("
                 + "fname VARCHAR(255) NOT NULL,"
@@ -63,6 +63,8 @@ public class DB {
                 + "FOREIGN KEY (email) REFERENCES users(email),"
                 + "FOREIGN KEY (movieName) REFERENCES movies(name))"
             ));
+
+            stmts.add(conn.prepareStatement("INSERT INTO users (fname, lname, email, dob, password, role) SELECT 'user', 'admin', 'ua', 'ua', 'ua', 'Admin' FROM dual WHERE NOT EXISTS (SELECT * FROM users WHERE email = 'ua');"));
 
             
             stmts.forEach(stmt -> {
