f73a05a33346222b30727df17930e66da07a0456
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-04-24 14:04:23 +0800

    chore: removed useless functions

--- Main/Entity/User.java
+++ Main/Entity/User.java
@@ -30,54 +30,6 @@ public class User {
         this.password = password;
         this.role = role;
     }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public void setFname(String fname) {
-        this.fname = fname;
-    }
-
-    public String getLname() {
-        return lname;
-    }
-
-    public void setLname(String lname) {
-        this.lname = lname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
    
     public DB getDB() {
         return db;
