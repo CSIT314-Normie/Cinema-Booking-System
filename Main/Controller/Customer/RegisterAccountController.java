c64b6a1f6ee2e59948ef761f597679877df2254c
ehjujube <yinli025@gmail.com>
2023-05-10 21:28:48 +0800

    fix

--- Main/Controller/Customer/RegisterAccountController.java
+++ Main/Controller/Customer/RegisterAccountController.java
@@ -6,29 +6,21 @@ import Main.Entity.*;
 
 import java.util.*;
 
-/**
- * This controller handles the registration of new accounts
- * USED BY CUSTOMER
- */
-public class RegisterAccountController {
-    private Customer customer;
 
-    /**
-     * Constructor for the RegisterAccountController class
-     * @param values ArrayList of account information
-     * @param role String of the role of the account
-     */
+public class RegisterAccountController {
+    private final Customer customer;
+
     public RegisterAccountController(ArrayList<String> values, String role) {
         customer = new Customer(values.get(0), values.get(1), values.get(2), values.get(3), values.get(4), role);
     }
 
-    /**
-     * Creates a new account by calling the createUser method in the Customer class
-     * @param values ArrayList of account information
-     * @param role String of the role of the account
-     * @return boolean of whether the account was created or not
-     */
+
     public boolean createUser(ArrayList<String> values, String role) {
         return this.customer.createUser(values, role);
-    } 
+    }
+
+    public boolean updateUser(ArrayList<String> values, String role) {
+        return this.customer.updateUser(values, role);
+    }
+
 }
