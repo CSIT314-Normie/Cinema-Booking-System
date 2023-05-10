a9de4ab97dba333150cd58770bc95009095904de
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-05-10 21:02:07 +0800

    fix: compilation issues

--- Main/Controller/LoginController.java
+++ Main/Controller/LoginController.java
@@ -91,7 +91,7 @@ public class LoginController {
 
     /**
      * This method is used to log out a user
-      * @param userRole is the role of the user
+     * @param userRole is the role of the user
      * @return true if the user is logged out, false otherwise
      */
     public boolean logout(String userRole) {
