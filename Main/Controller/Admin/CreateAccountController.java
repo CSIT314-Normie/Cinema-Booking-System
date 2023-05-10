e2bd29779ce45ae9fc11d23f6a558cce4ba97d03
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-05-10 20:54:21 +0800

    fix: added comments for each functions for clarity

--- Main/Controller/Admin/CreateAccountController.java
+++ Main/Controller/Admin/CreateAccountController.java
@@ -6,7 +6,11 @@ import Main.Entity.*;
 
 import java.util.*;
 
-// This controller handles the creation of accounts - Done by the USER ADMIN
+
+/**
+ * This controller handles the creation of accounts
+ * USED BY USER ADMIN
+ */
 
 public class CreateAccountController {
     private final UserAdmin user;
@@ -15,10 +19,21 @@ public class CreateAccountController {
         this.user = new UserAdmin();
     }
 
+    /**
+     * Creates a new account by calling the createUser method in the UserAdmin class
+     * @param accountInfo ArrayList of account information
+     * @param role String of the role of the account
+     * @return boolean of whether the account was created or not
+     */
     public boolean createAccount(ArrayList<String> accountInfo, String role) {
         return this.user.createUser(accountInfo, role);
     }
 
+    /**
+     * Validates the email by calling the validateEmail method in the UserAdmin class
+     * @param email String of the email to be validated
+     * @return boolean of whether the email is used or not
+     */
     public boolean validateEmail(String email) {
         return this.user.validateEmail(email);
     }
--- Main/Controller/Admin/SuspendAccountController.java
+++ Main/Controller/Admin/SuspendAccountController.java
@@ -1,10 +1,14 @@
 package Main.Controller.Admin;
 
-import Main.Entity.UserAdmin;
+import Main.Entity.*;
+
 
 /**
- * This is Admins Suspend Account Controller
+ * This controller handles the creation of accounts
+ * USED BY USER ADMIN
  */
+
+
 public class SuspendAccountController {
 
     private final UserAdmin userAdmin = new UserAdmin();
@@ -13,6 +17,11 @@ public class SuspendAccountController {
     public SuspendAccountController() {
     }
 
+    /**
+     * Suspends an account by calling the suspendAccount method in the UserAdmin class
+     * @param userEmail String of the email of the account to be suspended
+     * @return boolean of whether the account was suspended or not
+     */
     public boolean suspendAccount(String userEmail) {
         return this.userAdmin.suspendAccount(userEmail);
     }
