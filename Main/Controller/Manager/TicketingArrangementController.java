a3a11fbbdafd8ceab2045280cf90d219dc5fe9d9
ENJOYDEPROCESS <87604989+ENJOYDEPROCESS@users.noreply.github.com>
2023-05-11 16:29:48 +0800

    Created updateTicketController.java

--- Main/Controller/Manager/TicketingArrangementController.java
+++ Main/Controller/Manager/TicketingArrangementController.java
@@ -6,7 +6,6 @@ import java.util.*;
 
 /* 
 * This controller handles the ticketing arrangement information
-* - update of ticket price - CINEMA MANAGER
 * - get ticketing arrangement - CINEMA MANAGER
 */
 
@@ -24,14 +23,4 @@ public class TicketingArrangementController {
         return ticket.getTicketingArrangement(); 
     }
     
-    /**
-     * To handle the update of movie information - CINEMA MANAGER
-     * @param role only cinema manager can update ticket price 
-     * @param type type of ticket
-     * @param price price of ticket
-     * @return boolean if the update is successful
-     */
-    public boolean updateTicketPrice(String type, String price) { 
-        return ticket.updateTicketPrice(type, price);
-    }
 }
---
+++ Main/Controller/Manager/UpdateTicketController.java
@@ -0,0 +1,28 @@
+package Main.Controller.Manager;
+
+import Main.Entity.*;
+
+import java.util.*;
+
+/* 
+* This controller handles the update ticket information
+* - update of ticket price - CINEMA MANAGER
+*/
+
+public class UpdateTicketController {
+    Ticket ticket = new Ticket();
+    
+
+    public UpdateTicketController() {}
+    
+    /**
+     * To handle the update of movie information - CINEMA MANAGER
+     * @param role only cinema manager can update ticket price 
+     * @param type type of ticket
+     * @param price price of ticket
+     * @return boolean if the update is successful
+     */
+    public boolean updateTicketPrice(String type, String price) { 
+        return ticket.updateTicketPrice(type, price);
+    }
+}
