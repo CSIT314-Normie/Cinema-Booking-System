32af51e6e9649cbdf7c998ac6f9a382fe07d1866
MM <66454453+maheroxx@users.noreply.github.com>
2023-05-11 13:06:21 +0800

    feat: ReportB

---
+++ Main/Boundary/CinemaOwner/ReportB.java
@@ -0,0 +1,18 @@
+package Main.Boundary.CinemaOwner;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+
+public class ReportB extends JFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+    
+}
