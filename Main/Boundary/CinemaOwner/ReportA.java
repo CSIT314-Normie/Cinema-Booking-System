4455509f3d0eb5ce8236e3d6ceecfee6d302f53f
pnpdg <nicoledeg98@gmail.com>
2023-05-13 13:31:32 +0800

    fixed: Report A

--- Main/Boundary/CinemaOwner/ReportA.java
+++ Main/Boundary/CinemaOwner/ReportA.java
@@ -24,20 +24,15 @@ public class ReportA extends JFrame implements ActionListener
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
+        // combobox for the type of report for ReportA (Revenue for each cinema)
+        String[] typeOfReportA = {"Hourly", "Daily", "Weekly"};
+
+        JComboBox ReportAList = new JComboBox(typeOfReportA);
         
-
-
-           // combobox for the type of report for ReportA (Revenue for each cinema)
-           String[] typeOfReportA = {"Hourly", "Daily", "Weekly"};
-
-           JComboBox ReportAList = new JComboBox(typeOfReportA);
-           ReportAList.setSelectedIndex(3);
-           ReportAList.addActionListener(this);
         
         this.add(ReportAList);
-
         this.pack();
-        this.setVisible(true);
+        setSize(1035, 750);
     }
     
     // action listener for comboBox
