95b1a7ac07cf3666fd8c7e8a98d98379dc9f9539
ehjujube <yinli025@gmail.com>
2023-05-17 14:36:37 +0800

    fix: updated imports

--- Main/Boundary/Customer/SeatingPlan.java
+++ Main/Boundary/Customer/SeatingPlan.java
@@ -1,20 +1,11 @@
 package Main.Boundary.Customer;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList; 
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import Main.Boundary.Home;  
 
