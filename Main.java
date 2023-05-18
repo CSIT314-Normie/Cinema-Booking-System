0174f5b46a4bc3cc4a113b4ff4d60825c135b3b9
Min Zhan <26741875+chromanite@users.noreply.github.com>
2023-05-18 21:58:08 +0800

    bob the builder

--- Main.java
+++ Main.java
@@ -30,7 +30,8 @@ public class Main {
             "Main/Controller/Manager/*.java",
 
             // Main/Entity java files
-            "Main/Entity/*.java"
+            "Main/Entity/*.java",
+
         };
 
         // print out cmd as a string
@@ -81,5 +82,6 @@ public class Main {
 }
 
 // Manual compilation and running
-// javac -classpath "./Out/;./lib/mysql-connector-j-8.0.32.jar;./lib/jcalendar-1.4.jar" -d out Database/DB.java Main/Driver.java Main/Boundary/*.java Main/Boundary/Admin/*.java Main/Boundary/Owner/*.java Main/Boundary/Customer/*.java Main/Boundary/Manager/*.java Main/Controller/*.java Main/Controller/Admin/*.java Main/Controller/Customer/*.java Main/Controller/Manager/*.java Main/Entity/*.java 
-//  java -classpath "./Out;./lib/mysql-connector-j-8.0.32.jar" Main.Driver 
+// New
+// javac -classpath ".;./Out/;lib\mysql-connector-j-8.0.32.jar;lib\jcalendar-1.4.jar" -d Out Database/*.java Main/*.java Main/Boundary/*.java Main/Boundary/Admin/*.java Main/Boundary/Customer/*.java Main/Boundary/Manager/*.java Main/Boundary/Owner/*.java Main/Controller/*.java Main/Controller/Admin/*.java Main/Controller/Customer/*.java Main/Controller/Manager/*.java Main/Controller/Owner/*.java Main/Entity/*.java 
+// java -classpath ".;./Out;lib\mysql-connector-j-8.0.32.jar;lib\jcalendar-1.4.jar" Main.Driver
