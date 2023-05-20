9ec2086d7b8ffc506175492c3542debf35626a0f
MM <66454453+maheroxx@users.noreply.github.com>
2023-05-21 07:34:43 +0800

    b

--- Main/Controller/Owner/ReportBController.java
+++ Main/Controller/Owner/ReportBController.java
@@ -1,51 +1,85 @@
 package Main.Controller.Owner;
 
 import java.time.LocalDate;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import Main.Entity.*;
+import java.util.*;
 import Main.Boundary.Owner.ReportB;
 
 
 public class ReportBController {
 
-    public ReportB generateDailyReport() throws Exception
-    {
-        try{
-            LocalDate now = LocalDate.now();  
-            ReportB reportb = new ReportB(now.toString());
-            reportb.generateDailyReport();
-            return reportb;
+    public class DailyReportBController {
+        Visitor visitor = new Visitor();
+    
+        public DailyReportBController(){}
+        /**
+         * @return dailyReport
+         */
+        public HashMap<String, ArrayList<String>> getDailyReport(String monthYear) {
+            HashMap<String, ArrayList<String>> dailyReport = visitor.getDailyReport(monthYear);
+            return dailyReport;
         }
-        catch(Exception e)
-        {
-            throw e;
-        }
+        
     }
 
-    public ReportB generateWeeklyReport() throws Exception
-    {
-        try{
-            LocalDate now = LocalDate.now();  
-            ReportB reportb = new ReportB(now.toString());
-            reportb.generateWeeklyReport();
-            return reportb;
+    public class WeeklyReportBController {
+        Visitor visitor = new Visitor();
+    
+        public WeeklyReportBController(){}
+        /**
+         * @return weeklyReport
+         */
+        public HashMap<String, ArrayList<String>> getWeeklyReport(String monthYear) {
+            HashMap<String, ArrayList<String>> weeklyReport = visitor.getWeeklyReport(monthYear);
+            return weeklyReport;
         }
-        catch(Exception e)
-        {
-            throw e;
-        }
+        
     }
 
-    public ReportB generateMonthlyReport() throws Exception
-    {
-        try{
-            LocalDate now = LocalDate.now();  
-            ReportB reportb = new ReportB(now.toString());
-            reportb.generateMonthlyReport();
-            return reportb;
+    public class MonthlyReportBController {
+        Visitor visitor = new Visitor();
+    
+        public MonthlyReportBController(){}
+        /**
+         * @return monthlyReport
+         */
+        public HashMap<String, ArrayList<String>> getMonthlyReport(String year) {
+            HashMap<String, ArrayList<String>> monthlyReport = visitor.getMonthlyReport(year);
+            return monthlyReport;
         }
-        catch(Exception e)
-        {
-            throw e;
-        }
+        
     }
+    
+    
+    
+
+    // public ReportB generateWeeklyReport() throws Exception
+    // {
+    //     try{
+    //         LocalDate now = LocalDate.now();  
+    //         ReportB reportb = new ReportB(now.toString());
+    //         reportb.generateWeeklyReport();
+    //         return reportb;
+    //     }
+    //     catch(Exception e)
+    //     {
+    //         throw e;
+    //     }
+    // }
+
+    // public ReportB generateMonthlyReport() throws Exception
+    // {
+    //     try{
+    //         LocalDate now = LocalDate.now();  
+    //         ReportB reportb = new ReportB(now.toString());
+    //         reportb.generateMonthlyReport();
+    //         return reportb;
+    //     }
+    //     catch(Exception e)
+    //     {
+    //         throw e;
+    //     }
+    // }
 }
