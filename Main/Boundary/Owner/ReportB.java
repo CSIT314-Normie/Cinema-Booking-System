0aa00c53e1dc45f4a6c34879d726bd725479e4e6
MM <66454453+maheroxx@users.noreply.github.com>
2023-05-21 19:15:49 +0800

    report

--- Main/Boundary/Owner/ReportB.java
+++ Main/Boundary/Owner/ReportB.java
@@ -2,30 +2,43 @@ package Main.Boundary.Owner;
 
 import java.awt.*;
 import java.awt.event.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.time.Month;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+
 import java.awt.BorderLayout;
 
-import javax.management.modelmbean.ModelMBean;
 import javax.swing.*;
 
-import org.jfree.data.xy.XYSeries;
 import org.jfree.chart.ChartFactory; 
 import org.jfree.chart.ChartPanel;  
 
-import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 
-import org.jfree.chart.JFreeChart;  
-import org.jfree.data.category.DefaultCategoryDataset;  
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset; 
+
+import Main.Controller.Owner.*;
 
 
 public class ReportB extends JFrame implements ActionListener {
 
     JComboBox modeList;
     String currentMode;
-    DefaultCategoryDataset dataset; 
-    JFreeChart lineChart;
+    JDateChooser dateChooser;
+
     ChartPanel chartPanel;
+    DefaultCategoryDataset dataset; 
+    JFreeChart chart;
 
     public ReportB() {
         super("Report B");
@@ -37,6 +50,8 @@ public class ReportB extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setVisible(true);
 
+        // Create dataset
+        dataset = new DefaultCategoryDataset();
 
         JPanel modePanel = new JPanel();
         modePanel.setBackground(Color.LIGHT_GRAY);
@@ -55,107 +70,108 @@ public class ReportB extends JFrame implements ActionListener {
         JPanel reportPanel = new JPanel();
         reportPanel.setBackground(Color.GRAY );
         reportPanel.setSize(900,600);
-        
-        chartPanel = new ChartPanel(createChart(createDataset()));  
-        
 
-        JDateChooser dateChooser = new JDateChooser();
+        dateChooser = new JDateChooser();
         dateChooser.setLocale(Locale.US);
-        
-        JPanel datePanel = new JPanel();
         dateChooser.setPreferredSize(new Dimension(250, 30));
+  
+
+        // btn to trigger the combobox and chart
+        JButton confirmBtn = new JButton("Confirm");
+        confirmBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateChart();
+            }
+        });
+
+        JPanel datePanel = new JPanel();
         datePanel.add(new JLabel("Select :"));
         datePanel.add(dateChooser);
+        datePanel.add(confirmBtn);
 
+        // Create chart
+        JFreeChart chart = ChartFactory.createBarChart(
+                                "SeatID vs. Date",    // Chart title
+                                "Date",               // X-Axis label
+                                "SeatID",             // Y-Axis label
+                                dataset,              // Dataset
+                                PlotOrientation.VERTICAL,
+                                true,
+                                true,
+                                false
+        );
+
+        // Create chart panel
+        chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new Dimension(900,500));
 
         reportPanel.add(datePanel,BorderLayout.PAGE_START);
         reportPanel.add(chartPanel, BorderLayout.PAGE_END);
 
+
         this.add(modePanel,BorderLayout.PAGE_START);
         this.add(reportPanel,BorderLayout.CENTER);
     }
 
-    public ReportB(String string) {
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        //throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
         if (e.getSource()==modeList){
             System.out.println(modeList.getSelectedItem());
             currentMode = modeList.getSelectedItem().toString();
-            if(currentMode == "Select"){
-                dataset = new DefaultCategoryDataset();
-    
-            }else if(currentMode != "Select"){
-                chartPanel = new ChartPanel(createChart(createDataset()));
-                System.out.println("dataset");
-            }
-    
         }
     }
-    //call controller and load dataset
-    private DefaultCategoryDataset createDataset() {  
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();  
-
-        /**
-         * dataset.addValue(dailyVisitors, mode, )
-         */
-        dataset.addValue(200, "series1", "2016-12-19");  
-        dataset.addValue(150, "series1", "2016-12-20");  
-        dataset.addValue(100, "series1", "2016-12-21");  
-        dataset.addValue(210, "series1", "2016-12-22");  
-        dataset.addValue(240, "series1", "2016-12-23");  
-        dataset.addValue(195, "series1", "2016-12-24");  
-        dataset.addValue(245, "series1", "2016-12-25");  
-       
-        return dataset;  
-      }  
-      /**
-       * Todo: get the date on select 
-       * 
-       */
 
     /**
-     * call controller 
-     * 
+     * Replot the chart
      */
-    private DefaultCategoryDataset loadData(String mode, String date) {  
+    private void updateChart(){
+        dataset.clear();
+        // Get selected date from the date chooser
+        Date selectedDate = dateChooser.getDate();
+        if(currentMode == "Daily"){
+            DateFormat monthYearFormat = new SimpleDateFormat("MM/yyyy");
+            String monthYear = monthYearFormat.format(selectedDate).toString();
+            DailyReportBController dailyReportController = new DailyReportBController();
+            HashMap<String, ArrayList<String>> dailyReport = dailyReportController.getDailyReport(monthYear);
+            List<String> dates = new ArrayList<>();
+            List<Double> seatID = new ArrayList<>();
 
-      
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();  
+            for(String x: dailyReport.get("date")){
+                dates.add(x);
+                
+            }
+            for(String x: dailyReport.get("seatID")){
+                Double convertSeatID = Double.parseDouble(x);
+                seatID.add(convertSeatID); 
+            }
 
-        /**
-         * dataset.addValue(dailyVisitors, mode, )
-         */
-        dataset.addValue(200, "series1", "2016-12-19");  
-        dataset.addValue(150, "series1", "2016-12-20");  
-        dataset.addValue(100, "series1", "2016-12-21");  
-        dataset.addValue(210, "series1", "2016-12-22");  
-        dataset.addValue(240, "series1", "2016-12-23");  
-        dataset.addValue(195, "series1", "2016-12-24");  
-        dataset.addValue(245, "series1", "2016-12-25");  
-       
-      
-        return dataset;  
-      }
-      private JFreeChart createChart(DefaultCategoryDataset dataset) {
-        final  JFreeChart lineChart = ChartFactory.createLineChart(  
-            "Visitors Report", // Chart title  
-            "Date", // X-Axis Label  
-            "No. of visitors", // Y-Axis Label  
-            dataset  //dataset
-        );  
-        return lineChart;
+            for (int i = 0; i < dates.size(); i++) {
+                dataset.addValue(seatID.get(i), "SeatID", dates.get(i));
+            }
+
+        }else if(currentMode == "Monthly"){
+            DateFormat monthFormat = new SimpleDateFormat("yyyy");
+            String year = monthFormat.format(selectedDate).toString();
+            MonthlyReportBController monthlyReportController = new MonthlyReportBController();
+            HashMap<String, ArrayList<String>> monthlyReport = monthlyReportController.getMonthlyReport(year);
+            List<String> month = new ArrayList<>();
+            List<Double> seatID = new ArrayList<>();
+
+            for(String x: monthlyReport.get("month")){
+                int monthNumber = Integer.parseInt(x);
+                String monthName = Month.of(monthNumber).name();
+                month.add(monthName);
+                
+            }
+            for(String x: monthlyReport.get("seatID")){
+                Double convertSeatID = Double.parseDouble(x);
+                seatID.add(convertSeatID); 
+            }
+            for (int i = 0; i < month.size(); i++) {
+                dataset.addValue(seatID.get(i), "seatID", month.get(i));
+            }
+        }
+        chartPanel.repaint();
     }
-
-    public void generateDailyReport() {
-    }
-
-    public void generateMonthlyReport() {
-    }
-
-    public void generateWeeklyReport() {
-    }  
 }
