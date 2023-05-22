6d2d0c43b1b9e9968aabe9f045998ae1a2d36a62
ehjujube <60127640+ehjujube@users.noreply.github.com>
2023-05-22 14:39:08 +0800

    Added report B controller files

---
+++ Main/Controller/Owner/DailyReportBController.java
@@ -0,0 +1,19 @@
+package Main.Controller.Owner;
+import Main.Entity.*;
+import java.util.*;
+
+public class DailyReportBController {
+    Owner owner = new Owner();
+
+    public DailyReportBController(){}
+    
+    /**
+     * To get daily visitors 
+     * @return HashMap<String, ArrayList<String>> dailyReport
+     */
+    public HashMap<String, ArrayList<String>> getDailyReport(String monthYear) {
+        HashMap<String, ArrayList<String>> dailyReport = owner.getDailyVisitorsReport(monthYear);
+        return dailyReport;
+    }
+
+}
